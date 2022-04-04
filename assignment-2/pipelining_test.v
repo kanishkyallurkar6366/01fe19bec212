b29e300d49a4738dbc80ebb08093ddb5cb0a4e74
kanishkyallurkar6366 <59930656+kanishkyallurkar6366@users.noreply.github.com>
2022-04-04 21:34:54 +0530

    Update pipelining_test.v

--- assignment-2/pipelining_test.v
+++ assignment-2/pipelining_test.v
@@ -1,27 +1,3 @@
-`timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   09:13:45 03/26/2022
-// Design Name:   pipelining
-// Module Name:   C:/project/assignment2/pipelining_test.v
-// Project Name:  assignment2
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: pipelining
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module pipelining_test;
 
 	// Inputs
